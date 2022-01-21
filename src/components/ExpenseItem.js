52ad4ee06c5eb93a66a57ef978f34d45b4625a6b
yadavraju <abdul.h@chapter247.com>
2022-01-21 16:48:29 +0530

    splitting component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './expenseItem.css';
-
-function formatTime(time, prefix = "") {
-    return typeof time == "object" ? prefix + time.toLocaleDateString() : "";
-}
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    const month = props.date.toLocalString('en-US', { month: 'long' })
-    const year = props.date.toLocalString('en-US', { day: '2-digit' })
-    const day = props.date.getFullYear();
+    
     return(
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
